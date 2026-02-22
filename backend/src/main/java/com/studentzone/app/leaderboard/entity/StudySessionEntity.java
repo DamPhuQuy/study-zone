c6baaec7e44ea6c533411c038ee932fce21520fd
DamPhuQuy <phuquydam06@gmail.com>
2026-02-22 23:52:35 +0700

    feat: add study session entity

---
+++ backend/src/main/java/com/studentzone/app/leaderboard/entity/StudySessionEntity.java
@@ -0,0 +1,30 @@
+package com.studentzone.app.leaderboard.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "study_session")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudySessionEntity {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long pointsEarned;
+}
